a385eeafcf324c9cd93859182a4ac85eeb0ccbe7
06irish <98928828+06irish@users.noreply.github.com>
2022-02-22 12:37:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<html>
 <head>
-<title> Scaffold </title>
+<title> Performance Task </title>
 <style>
 body { 
   background: url(Background.gif) no-repeat center center fixed; 
@@ -16,10 +16,10 @@ body {
 <body link="green" vlink="yellow" alink="red" p align="right">
 <br>
 <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
-&nbsp &nbsp &nbsp &nbsp &nbsp
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a href="CAE.php">CAE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp
-<a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp &nbsp
+<a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
 
 </p> </font> </b>
 
