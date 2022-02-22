bb7c81977827801bddbeb96e488925048504b8ed
06irish <98928828+06irish@users.noreply.github.com>
2022-02-22 23:54:14 +0800

    Update CLE.php

--- CLE.php
+++ CLE.php
@@ -14,25 +14,28 @@
 <center>
 <table bgcolor="yellow" border="5">
   <tr>
-    <th> <font face="Times New Roman" size="14"> <b> CHRISTIAN LIVING EDUCATION </font> </b> </th>
+    <th> <font face="Times New Roman" size=14> <b> CHRISTIAN LIVING EDUCATION </font> </b> </th>
   </tr>
 </table>
 <br>
 <br>
-<font face="Broadway" size="7"> MISS LILIVETH GUSTILLO </font>
+<font face="Broadway" size=6> MISS LILIVETH GUSTILLO </font>
 
 <br>
 <br>
+  <br>
+  <br>
+  <br>
 
 <table bgcolor="#A32CC4" border="8" cellspacing="10">
   <tr>
      <th> 
-          <font face="Times New Roman" size="8"> 
+          <font face="Times New Roman" size=8> 
           <a id="GFG" href="https://docs.google.com/document/d/1BVrC26xK4ZANNlya0Ql_BHOeK63Ju_D7s7-MhMEnq_8/edit"> &nbsp Document &nbsp </a>
      </th>
    
      <th> 
-          <font face="Times New Roman" size="8"> 
+          <font face="Times New Roman" size=8> 
           <a id="GFG" href="https://grp19-scaffold.herokuapp.com/"> PowerPoint <br> Presentation </a>
      </th>
   </tr>
