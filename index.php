d5e417f4bc097a9166931c3c50942c6f5b13e0dc
06irish <98928828+06irish@users.noreply.github.com>
2022-02-22 13:00:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,11 +17,11 @@ body {
 </head>
 
    <body link="green" vlink="yellow" alink="red" p align="right">
-<font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
+<font face="Times New Roman" size=13> <b> <a id = "GFG" href="CLE.php">CLE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-<a href="CAE.php">CAE</a>
+<a id = "GFG" href="CAE.php">CAE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-<a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
+<a id = "GFG" href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
 
  </p> </font> </b>
 
