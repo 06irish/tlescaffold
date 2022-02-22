7ba17124f2747f3f61b241f2319eacb603ef805d
06irish <98928828+06irish@users.noreply.github.com>
2022-02-22 12:48:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,15 @@ body {
 </style>
 </head>
 
-<body link="green" vlink="yellow" alink="red" p align="right">
+<STYLE>A {text-decoration: none;} 
+   <body link="green" vlink="yellow" alink="red" p align="right">
 <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a href="CAE.php">CAE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
 
-</p> </font> </b>
+  </style> </p> </font> </b>
 
 </body>
 </html>
