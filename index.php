9a7ef22c090dde80e0041c78ad67273be43aef19
06irish <98928828+06irish@users.noreply.github.com>
2022-02-22 12:41:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ body {
 <body link="green" vlink="yellow" alink="red" p align="right">
 <br>
 <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
-&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a href="CAE.php">CAE</a>
-&nbsp &nbsp &nbsp &nbsp &nbsp
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
 
 </p> </font> </b>
