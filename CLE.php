504ad3d3541885fc39a040d5ec5c18295eb5ce13
06irish <98928828+06irish@users.noreply.github.com>
2022-02-26 00:55:55 +0800

    Update CLE.php

--- CLE.php
+++ CLE.php
@@ -1,56 +1,36 @@
 <html>
-<head> 
-  <title> CLE </title>
+<head>
+<title> The Roots and Fruits of Depression </title>
+
 <style>
-        #GFG {
-            text-decoration: none;
-        }
-
-</style>
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+}
+      a:link {
+  text-decoration: none;
+}
+a:visited {
+  text-decoration: none;
+}
+   </style>
 </head>
-<body bgcolor= "#A45EE5" link="black" vlink="black" alink="purple">
-<br>
-<br>
-<center>
-<table bgcolor="yellow" border="5">
-  <tr>
-    <th> <font face="Times New Roman" size=14> <b> CHRISTIAN LIVING EDUCATION </font> </b> </th>
-  </tr>
-</table>
-<br>
-  <br>
-<br>
-<font face="Broadway" size=6.5> MISS LILIVETH GUSTILLO </font>
 
-<br>
-<br>
-<br>
-  
- <font face="Courier" size=12 color="yellow"> <b>
-     The Roots and Fruits of Depression
-   </font> </b>
-  
-  <br>
-  <br>
-
-<table bgcolor="#A32CC4" border="8" cellspacing="10">
-  <tr>
-     <th> 
-          <font face="Times New Roman" size=8> 
-            Document<br>Form </font>
-            <br>
-            <font face="Times New Roman" size=5>
-          <a id="GFG" href="https://docs.google.com/document/d/1BVrC26xK4ZANNlya0Ql_BHOeK63Ju_D7s7-MhMEnq_8/edit">  Google Docs  </a>
-          <a id="GFG" href="The-Roots-and-Fruits-of-Depression.docx">  MS Word  </a>
-     </th>
+   <body bgcolor="green" link="green" vlink="yellow" alink="red" p align="right">
+<div class="container">
+  <img src="Background cle.png" alt="background" style="width:100%;">
+  <div class="top-right"> <br>
+      <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
+         
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
    
-     <th> 
-          <font face="Times New Roman" size=8> 
-          <a id="GFG" href="The-Roots-and-Fruits-of-Depression.pdf"> &nbsp Poster &nbsp <br> &nbsp Form &nbsp </a>
-     </th>
-  </tr>
-</table>
-
-</center>
+            <a href="CAE.php"> CAE </a>
+         
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   
+    <a href="MATH.php">MATH</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </div>
+ </p> </font> </b> </div>
 </body>
 </html>
