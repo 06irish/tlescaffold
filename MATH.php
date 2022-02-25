22ed27e0c6ac2ebfe8379a27e023bc8de8eb0af9
06irish <98928828+06irish@users.noreply.github.com>
2022-02-26 03:29:42 +0800

    Update MATH.php

--- MATH.php
+++ MATH.php
@@ -1,20 +1,33 @@
 <html>
-<head> 
-  <title> MATH </title>
-</head>
-<body bgcolor= "red">
-<br>
-<br>
-<center>
-<table bgcolor="white" border="5">
-  <tr>
-    <th> <font face="Times New Roman" size=16> <b> MATHEMATICS </font> </b> </th>
-  </tr>
-</table>
-<br>
-<br>
-<font face="Broadway" size="10"> Sir Pax Valor </font>
+<head>
+<title> Message of Hope </title>
 
-</center>
+<style>
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.centered {
+  position: absolute;
+  top: 59%;
+  left: 64.5%;
+  transform: translate(-50%, -50%);
+}
+      a:link {
+  text-decoration: none;
+}
+a:visited {
+  text-decoration: none;
+}
+   </style>
+</head>
+
+   <body link="white" vlink="green" alink="black">
+<div class="container">
+  <img src="Background math.png" alt="background" style="width:100%;">
+   <div class="centered"> 
+   <font face="Times New Roman" size="6">
+     <a href="https://www.canva.com/design/DAE5EyTx4-Y/G9jRmfuZsb270bXGo3Nk-w/edit"> Click Here </a></div>
+ </font> </b> </div>
 </body>
-</html>
