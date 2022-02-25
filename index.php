1c81b8d817f34361313e7e08682f0cd9dcac4e54
06irish <98928828+06irish@users.noreply.github.com>
2022-02-25 21:05:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 ﻿<html>
 <head>
 <title> Performance Task </title>
+
+   <style>
+      a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+   </style>
 </head>
 
    <body bgcolor="green" link="green" vlink="yellow" alink="red" p align="right">
-<img src="1.gif">
-<font face="Times New Roman" size=13> <b> <a href="CLE.php" style="text-decoration: none;>CLE</a>
-&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+
+      <img src="Backgroundd.gif">
+
+      <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
+         
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  
    
-<a href="CAE.php" style="text-decoration: none;>CAE</a>
+ <a href="CAE.php"> 
+         
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
    
-<a href="MATH.php" style="text-decoration: none;>MATH</a> &nbsp &nbsp &nbsp &nbsp
+<a href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
 
  </p> </font> </b>
 
