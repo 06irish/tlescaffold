782a71c2e757227be4f94638c2b96e7a2e94b352
06irish <98928828+06irish@users.noreply.github.com>
2022-02-26 01:00:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ a:visited {
    <body bgcolor="green" link="green" vlink="yellow" alink="red" p align="right">
 
 <div class="container">
-  <img src="1.gif" alt="background" style="width:100%;height:100%">
+  <img src="1.gif" alt="background" style="width:100%;">
   <div class="top-right"> <br>
       <font face="Times New Roman" size=13> <b> <a href="CLE.php">CLE</a>
          
