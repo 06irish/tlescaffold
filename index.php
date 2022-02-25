e2d156b2b7d137bac522ba7e988675ccb4f8e583
06irish <98928828+06irish@users.noreply.github.com>
2022-02-25 20:43:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 ﻿<html>
 <head>
 <title> Performance Task </title>
-<style>
-
-        #GFG {
-            text-decoration: none;
-        }
-    
-</style>
 </head>
 
    <body bgcolor="green" link="green" vlink="yellow" alink="red" p align="right">
 <img src="Backgroundd.gif" width=100% height=100%>
-<font face="Times New Roman" size=13> <b> <a id = "GFG" href="CLE.php">CLE</a>
+<font face="Times New Roman" size=13> <b> <a href="CLE.php" style="text-decoration: none;>CLE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-<a id = "GFG" href="CAE.php">CAE</a>
+<a href="CAE.php" style="text-decoration: none;>CAE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-<a id = "GFG" href="MATH.php">MATH</a> &nbsp &nbsp &nbsp &nbsp
+<a href="MATH.php" style="text-decoration: none;>MATH</a> &nbsp &nbsp &nbsp &nbsp
 
  </p> </font> </b>
 
