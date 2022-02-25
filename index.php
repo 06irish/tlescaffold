2f8e4cc292902f92ccb0e171f42e23a508ee40a6
06irish <98928828+06irish@users.noreply.github.com>
2022-02-25 20:24:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
 <head>
 <title> Performance Task </title>
 <style>
-body { 
-  background: url(Background.gif) no-repeat center center fixed; 
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover; cover
-}
+
         #GFG {
             text-decoration: none;
         }
@@ -15,7 +10,8 @@ body {
 </style>
 </head>
 
-   <body link="green" vlink="yellow" alink="red" p align="right">
+   <body bgcolor="green" link="green" vlink="yellow" alink="red" p align="right">
+<img src="Backgroundd.gif" width=100% height=100%>
 <font face="Times New Roman" size=13> <b> <a id = "GFG" href="CLE.php">CLE</a>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
 <a id = "GFG" href="CAE.php">CAE</a>
